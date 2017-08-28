78bd76c07f9a32d77052e186438093c39c38cd18
William J Reid <willjreid@gmail.com>
2017-08-28 12:17:07 -0700

    adding contestant name feature

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+name.addEventListener('');
+var begin = document.getElementById('spin');
+
+function nameToPersist(event) {
+  var contestantName = input.username;
+  localStorage.setItem(persistedName = contestantName);
+}
+var elementUsername = document.getElementById('username');
+elementUsername.addListener('blur', checkUsername, false);
+//on the result page, recall contestant name:
+contestantName = localStorage.getItem(persistedName);
