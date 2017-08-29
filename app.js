57d019a76336e789840a98afd2561325e8820d55
Mark Reynoso <mreynoso@spu.edu>
2017-08-29 16:02:30 -0700

    attempting delayed spin on landing page wheel

--- app.js
+++ app.js
@@ -2,14 +2,19 @@
 
 //add contestant nanme
 var name = document.getElementById('contestant');
+name.addEventListener('submit', store);
 
 function store(event) {
   event.preventDefault();
   localStorage.setItem('name', event.target.name);
+  setTimeout(function() {
+    var spin = document.getElementById('wheel');
+    spin.style.-webkit-animation-name = spin;
+  }, 1000);
 };
+
 console.log('submit');
 console.log(name);
-name.addEventListener('submit', store);
 console.log('submit');
 console.log(name);
 
